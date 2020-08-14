ffa7f9d8caa69c3754bc4a7d756e24f32a0f8005
Pedro Abreu <abreu223@hotmail.com>
2020-08-14 14:52:01 -0400

    Comments piece of code

--- src/exp.ml
+++ src/exp.ml
@@ -452,6 +452,7 @@ and of_match
       let* cast = Type.decode_var_tags new_typ_vars None cast in
       let* motive = Type.decode_var_tags new_typ_vars None motive in
       let (cast, args) = Type.normalize_constructor cast in
+      (* Only generates dependent pattern matching for actual gadts *)
       if List.length args = 0
       then return None
       else return (Some ({cast; args; motive}))
